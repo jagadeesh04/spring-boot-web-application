0da44b1e38fe4436b805f8d7abb480364df30d8d
Jag Gangishetti <jagadeesh.g07@gmail.com>
2017-07-27 17:13:18 -0400

    implemented delete functionality

--- src/main/java/com/jag/springBoot/web/controller/TodoController.java
+++ src/main/java/com/jag/springBoot/web/controller/TodoController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.jag.springBoot.web.service.TodoService;
@@ -28,5 +29,20 @@ public class TodoController {
 	public String addTodos(ModelMap model){
 		return "add-todo";
 	}
+	
+	@RequestMapping(value="/delete-todo", method= RequestMethod.GET)
+	public String deleteTodos(@RequestParam int id){
+		service.deleteTodo(id);
+		return "redirect:/list-todos";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
